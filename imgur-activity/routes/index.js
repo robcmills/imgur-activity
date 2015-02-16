35c6394b10f4bea5c023484c554b352137661682
Robert Mills <robcmills@gmail.com>
2015-02-16 10:36:16 -0800

    move users routes to index for better routes hierarchy

--- imgur-activity/routes/index.js
+++ imgur-activity/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var users = require('./users');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/users', users);
 
 module.exports = router;
