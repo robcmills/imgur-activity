5b5388cf604fb80c32d65300658f9c0c1607b80d
Robert Mills <robcmills@gmail.com>
2015-02-16 12:34:08 -0800

    rename content block to main

--- imgur-activity/routes/index.js
+++ imgur-activity/routes/index.js
@@ -1,9 +1,10 @@
+
 var express = require('express');
 var users = require('./users');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'imgur activity' });
 });
 
 router.use('/users', users);
