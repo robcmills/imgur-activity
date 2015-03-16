4712c474cbfbe8f6cb262bbf30ace8ed386629d3
Robert Mills <robcmills@gmail.com>
2015-03-16 03:15:23 -0700

    add chart.js

--- ember-app/js/controllers.js
+++ ember-app/js/controllers.js
@@ -100,3 +100,39 @@ App.WatchesController = Ember.ArrayController.extend({
     }
   }
 });
+
+App.ActivityView = Ember.View.extend({
+  didInsertElement: function() {
+    var activityChart, ctx, options;
+    ctx = $("#activity-chart").get(0).getContext("2d");
+    options = {};
+    return activityChart = new Chart(ctx).Line(this.get('controller.data', options));
+  }
+});
+
+App.ActivityController = Ember.Controller.extend({
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }, {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }
+    ]
+  }
+});
